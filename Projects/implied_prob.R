0bfd466d260cdef05a63d6a1578c4c3c07e661bb
Jason Lee <Jason@AISportsFirm.com>
2020-07-07 14:42:36 -0700

    Implied Probability function

---
+++ Projects/implied_prob.R
@@ -0,0 +1,15 @@
+#' Implied Probability of Winning (American Odds)
+#'
+#' @param odds
+#'
+#' @return Implied probability of winning or the "Break Even" percentage
+#' @export
+#'
+#' @examples implied_prob(-150)
+#' @examples implied_prob(360)
+implied_prob <- function(odds){
+  ifelse(odds > 0, 
+         imp_prob <- 100 / (odds + 100), 
+         imp_prob <- -odds / (-odds + 100))
+  return(imp_prob)
+}
