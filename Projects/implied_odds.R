a4b21212a4173a10a46e180b34c8684d5cf2b21f
Jason Lee <Jason@AISportsFirm.com>
2020-07-10 22:30:10 -0700

    Implied Odds from Probability

--- Projects/implied_odds.R
+++ Projects/implied_odds.R
@@ -1,15 +1,37 @@
 #' Probability of Winning Conversion to Odds (American Odds)
 #'
 #' @param prob Probability of winning a bet
+#' @param type Odds Type ("us", "dec", "frac") "us" == American Odds, "dec" == Decimal Odds, "frac" == Fractual Odds
+#' 
 #'
 #' @return odds American Odds of that bet
 #' @export
 #'
 #' @examples implied_odds(0.4)
 #' @examples implied_odds(0.5238095)
-implied_odds <- function(prob){
-  ifelse(prob > 0.5, 
-         odds <- prob / (1 - prob) * -100, 
-         odds <- (1 - prob) / prob * 100)
+#' @examples implied_odds(c(0.3, 0.2, 0.909, 0.7143), type = "dec")
+#' @examples implied_odds(c(0.3, 0.2, 0.95, 0.7), type = "frac")
+
+implied_odds <- function(prob, type = "us"){
+  if (type == "us") {
+    odds <- prob
+    odds[] <- NA_real_
+    odds[which(prob > 0.5)] <- prob[which(prob > 0.5)] / (1 - prob[which(prob > 0.5)]) * -100
+    odds[which(prob <= 0.5)] <- (1 - prob[which(prob <= 0.5)]) / prob[which(prob <= 0.5)] * 100
+    odds
+  }
+  if (type == "dec") {
+    odds <- prob
+    odds[] <- NA_real_
+    odds[which(prob < 1)] <- 1 / prob[which(prob < 1)]
+    odds
+  } 
+  if (type == "frac") {
+    odds <- prob
+    odds[] <- NA_real_
+    odds[which(prob < 1)] <- 1 / prob[which(prob < 1)]
+    odds <- odds - 1
+    odds <- MASS::fractions(odds)
+  } 
   return(odds)
 }
